6d1601e5e21bd858f88792d4ed923f92c1b99731
Cristian Rojas <cristian.rojas@live.fr>
2025-07-23 15:24:47 +0200

    Initial Commit

---
+++ ContextualNavigationBar/ContentView.swift
@@ -0,0 +1,19 @@
+// © 2025  Cristian Felipe Patiño Rojas. Created on 23/7/25.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ContextualNavigationBar/ContextualNavigationBarApp.swift
@@ -0,0 +1,12 @@
+// © 2025  Cristian Felipe Patiño Rojas. Created on 23/7/25.
+
+import SwiftUI
+
+@main
+struct ContextualNavigationBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
